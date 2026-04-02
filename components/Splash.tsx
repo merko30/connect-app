258281c2173dc0b4b671774c2bae0c95f070c112
Merim <merim.hasanbegovic@outlook.com>
2026-04-02 07:43:26 +0200

    fix: splash screen bg
    Signed-off-by: Merim <merim.hasanbegovic@outlook.com>

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -1,10 +1,15 @@
 import Logo from "@/assets/images/logo.svg";
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet } from "react-native";
+import { StyleSheet, useColorScheme } from "react-native";
 
 const Splash = () => {
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === "dark";
   return (
-    <LinearGradient colors={["#FFFFFF", "#F2F2F2"]} style={styles.container}>
+    <LinearGradient
+      colors={isDarkMode ? ["#000000", "#1A1A1A"] : ["#FFFFFF", "#F2F2F2"]}
+      style={styles.container}
+    >
       <Logo width={120} height={120} />
     </LinearGradient>
   );
